eb705a6b849e62147f4cd2762b472378b29dad99
SherryYijing <48372728+SherryYijing@users.noreply.github.com>
2023-04-19 09:45:36 -0400

    all done

--- app/src/main/java/com/example/cs5520_finalproject_group2/MapActivity.java
+++ app/src/main/java/com/example/cs5520_finalproject_group2/MapActivity.java
@@ -15,6 +15,8 @@ import android.location.Geocoder;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cs5520_finalproject_group2.Models.Event;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,14 +31,12 @@ import org.osmdroid.api.IMapController;
 import org.osmdroid.bonuspack.routing.OSRMRoadManager;
 import org.osmdroid.bonuspack.routing.Road;
 import org.osmdroid.bonuspack.routing.RoadManager;
-import org.osmdroid.bonuspack.routing.RoadNode;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.Polyline;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
@@ -46,6 +46,7 @@ public class MapActivity extends AppCompatActivity {
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MyLocationNewOverlay myLocationOverlay;
     private MapView map = null;
+    private Button backButton;
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore db;
     private FirebaseUser currentUser;
@@ -79,6 +80,14 @@ public class MapActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        backButton = findViewById(R.id.mapBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         map = (MapView) findViewById(R.id.mapView);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
@@ -90,86 +99,26 @@ public class MapActivity extends AppCompatActivity {
         map.setScrollableAreaLimitLatitude(MapView.getTileSystem().getMaxLatitude(), MapView.getTileSystem().getMinLatitude(), 0);
         map.setScrollableAreaLimitLongitude(MapView.getTileSystem().getMinLongitude(), MapView.getTileSystem().getMaxLongitude(), 0);
 
-//        double DEFAULT_ZOOM = 3;
-//        map.getController().setZoom(DEFAULT_ZOOM);
-//        map.setMinZoomLevel(DEFAULT_ZOOM);
-//        GeoPoint centerPoint = new GeoPoint(42.3398, -71.0892);
-//        map.getController().setCenter(centerPoint);
-//        map.getController().setZoom(18);
         map.setMultiTouchControls(true);
 
-        GeoPoint centerPoint = new GeoPoint(42.3398, -71.0892);
         IMapController mapController = map.getController();
         mapController.setZoom(17);
-//        mapController.setCenter(centerPoint);
-
-
-//        List<GeoPoint> geoPoints = new ArrayList<>();
-//        // trip.json provided from professor.
-//        String jsonfromTrip = "";
-//        InputStream inputStream =
-//                getResources().openRawResource(R.raw.trip);
-//        try {
-//            byte[] buffer = new byte[inputStream.available()];
-//            while(inputStream.read(buffer) != -1);
-//            jsonfromTrip = new String(buffer);
-//            JSONObject rootJSON = null;
-//            try {
-//                rootJSON = new JSONObject(jsonfromTrip);
-//                JSONArray arrayJSON = rootJSON.getJSONArray("locations");
-//                for (int i = 0; i < arrayJSON.length(); i++) {
-//                    JSONObject itemJSON = arrayJSON.getJSONObject(i);
-//                    double latitude = itemJSON.getDouble("latitude");
-//                    double longitude = itemJSON.getDouble("longitude");
-//                    geoPoints.add(new GeoPoint(latitude, longitude));
-//                }
-//            } catch (JSONException e) {
-//                throw new RuntimeException(e);
-//            }
-//            inputStream.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-//        Polyline line = new Polyline();   //see note below!
-//        line.setPoints(geoPoints);
-//        map.getOverlayManager().add(line);
-
-//        Polygon polygon = new Polygon();
-//        geoPoints.add(geoPoints.get(0));
-//        polygon.getFillPaint().setColor(Color.parseColor("#1EFFE70E")); //set fill color
-//        polygon.setPoints(geoPoints);
-//        polygon.setTitle("Northeastern University");
-//        map.getOverlayManager().add(polygon);
-
-        // Navigation
-//        this.myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this),map);
-//        this.myLocationOverlay.enableMyLocation();
-        // Create start point.
 
         // Use getGeocode function to get latitude and longitude.
         Address address = getGeocode("Northeastern University");
         // Create start point.
         GeoPoint startPoint = new GeoPoint(address.getLatitude(), address.getLongitude());
         Marker startMarker = new Marker(map);
-//        GeoPoint startPoint = new GeoPoint(42.3385434,-71.0971816);
         startMarker.setPosition(startPoint);
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         map.getOverlays().add(startMarker);
         map.invalidate();
         startMarker.setTitle("Start point");
 
-//        Marker startMarker = new Marker(map);
-//        GeoPoint startPoint = new GeoPoint(42.3385434,-71.0971816);
-//        startMarker.setPosition(startPoint);
-//        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-//        map.getOverlays().add(startMarker);
-//        map.invalidate();
-//        startMarker.setTitle("Start point");
-
         // Make the center of the map to be the start point.
         mapController.setCenter(startPoint);
 
+        // Get event from database.
         Intent intent = getIntent();
         String day = intent.getStringExtra("data");
         ArrayList<Event> eventList = new ArrayList<>();
@@ -212,34 +161,6 @@ public class MapActivity extends AppCompatActivity {
                     }
                 });
 
-//        // Create stop point.
-//        Marker endMarker = new Marker(map);
-//        GeoPoint endPoint = new GeoPoint(42.3395109,-71.0913559);
-//        endMarker.setPosition(endPoint);
-//        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-//        map.getOverlays().add(endMarker);
-//        map.invalidate();
-//        endMarker.setTitle("End point");
-//
-//        // Get road between start point and end point.
-//        RoadManager roadManager = new OSRMRoadManager(this, getPackageName());
-//        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
-//        waypoints.add(startPoint);
-//        waypoints.add(endPoint);
-//        Road road = roadManager.getRoad(waypoints);
-//        Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
-//        map.getOverlays().add(roadOverlay);
-//        map.invalidate();
-//
-//        // Mark each step.
-//        for (int i=0; i < road.mNodes.size(); i++){
-//            RoadNode node = road.mNodes.get(i);
-//            Marker nodeMarker = new Marker(map);
-//            nodeMarker.setPosition(node.mLocation);
-//            nodeMarker.setTitle("Step "+ i);
-//            map.getOverlays().add(nodeMarker);
-//        }
-
         requestPermissionsIfNecessary(new String[]{
                 // if you need to show the current location, uncomment the line below
                 Manifest.permission.ACCESS_FINE_LOCATION,
@@ -272,20 +193,14 @@ public class MapActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+        map.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
         //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        map.onPause();
     }
 
     @Override
